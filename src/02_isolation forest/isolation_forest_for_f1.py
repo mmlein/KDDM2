be29a19405fa57b31f089985537f7c786c4f5818
the_datre <dt@mailexpress.cc>
2023-01-22 14:55:21 +0100

    Changes for isolation forest

--- src/02_isolation forest/isolation_forest_for_f1.py
+++ src/02_isolation forest/isolation_forest_for_f1.py
@@ -10,17 +10,22 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 from pyod.models.iforest import IForest
 import string
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
 
 import warnings
 warnings.filterwarnings("ignore")
 
 path_data = Path("Data/dataframe_with_outliers_3std_4.16%.csv").absolute()
 
-letter_df_complete = pd.read_csv(path_data)
+columns = ["", "letter", "x-box", "y-box", "width", "high", "onpix", "x-bar", "y-bar", "x2bar",
+        "y2bar", "xybar", "x2ybr", "xy2br", "x-ege", "xegvy", "y-ege", "yegvx", "outlier"]
 
-columns = letter_df_complete.columns.values.tolist()
+letter_df_complete = pd.read_csv(path_data, names=columns)
+
+#columns = letter_df_complete.columns.values.tolist()
 columns_train = columns[2:-1]
 add_columns = ["Anomaly_Score", "projection"]
+#columns_df_res = columns + add_columns
 columns_df_res = columns + add_columns
 columns_df_res.remove("letter")
 
@@ -50,13 +55,14 @@ def prepare_dataset(letter: str, dataset: pd.DataFrame, test_prob: float = 0.2,
 
     # Split the dataset into training and test data
     letter_train, letter_test = train_test_split(
-        dataset_wo_let, test_size=test_prob, train_size = (1-test_prob), random_state=rand_state)
+        dataset_wo_let, test_size=test_prob, train_size=(1-test_prob), random_state=rand_state)
 
     return dataset_letter, letter_train, letter_test
 
+
 def isolation_forest(train_data: pd.DataFrame, test_data: pd.DataFrame, columns: list[str] = columns_train,
-    cont: float = 0.05, max_feat: int = 1.0, max_samp: int = 40, n_est: int = 100, 
-    random_state: int = 0) -> tuple[pyod.models.iforest, np.ndarray, np.ndarray]:
+                     cont: float = 0.05, max_feat: int = 1.0, max_samp: int = 40, n_est: int = 100,
+                     random_state: int = 0) -> tuple[pyod.models.iforest, np.ndarray, np.ndarray]:
     """
     Creates a new isolation forest.
 
@@ -91,6 +97,7 @@ def isolation_forest(train_data: pd.DataFrame, test_data: pd.DataFrame, columns:
 
     return isft, y_train_scores, y_test_scores, y_train_pred, y_test_pred
 
+
 def categorization(dataset: pd.DataFrame, pred_score: np.ndarray, threshold: float) -> pd.DataFrame:
     """
     Calculates the statistics (https://towardsdatascience.com/use-the-isolated-forest-with-pyod-3818eea68f08).
@@ -109,6 +116,7 @@ def categorization(dataset: pd.DataFrame, pred_score: np.ndarray, threshold: flo
         dataset['Anomaly_Score'] < threshold, 0, 1)
 
     return (dataset)
+
 
 def descriptive_stat_threshold(dataset: pd.DataFrame) -> tuple[int, int, float]:
     """
@@ -129,9 +137,10 @@ def descriptive_stat_threshold(dataset: pd.DataFrame) -> tuple[int, int, float]:
 
     return total_datapoints, total_outliers, percentage
 
+
 def algorithm(dataset_complete: pd.DataFrame, cont: float = 0.05, max_feat: int = 1.0,
-              max_samp: int = 40, n_est: int = 100, random_state: int = 0, write_to_csv: bool = False) -> tuple[pd.DataFrame, 
-              pd.DataFrame]:
+              max_samp: int = 40, n_est: int = 100, random_state: int = 0, write_to_csv: bool = False) -> tuple[pd.DataFrame,
+                                                                                                                pd.DataFrame]:
     """
     Performs all necessary steps for determining outliers.
 
@@ -164,13 +173,14 @@ def algorithm(dataset_complete: pd.DataFrame, cont: float = 0.05, max_feat: int 
     for letter in alphabet:
 
         # Prepare dataset
-        data_letter, letter_train, letter_test = prepare_dataset(letter, dataset_complete, test_prob=0.2)
-        
+        data_letter, letter_train, letter_test = prepare_dataset(
+            letter, dataset_complete, test_prob=0.2)
+
         # Perform the isolation forest
-        iso_for, y_train, y_test, y_train_pred, y_test_pred = isolation_forest(letter_train, letter_test, columns_train, 
-            cont, max_feat, max_samp, n_est, random_state)
+        iso_for, y_train, y_test, y_train_pred, y_test_pred = isolation_forest(letter_train, letter_test, columns_train,
+                                                                               cont, max_feat, max_samp, n_est, random_state)
         threshold_outlier = iso_for.threshold_
-        
+
         # Categorize outliers
         res_letter = categorization(letter_test, y_test, threshold_outlier)
         results_fi = pd.concat([results_fi, res_letter], ignore_index=True)
@@ -184,19 +194,28 @@ def algorithm(dataset_complete: pd.DataFrame, cont: float = 0.05, max_feat: int 
         results_abs_dict[letter] = num_out
         results_rel_dict[letter] = perc
 
+    # Save overall results
+    results_fi = results_fi.astype(float)
+    results_abs_dict["Total"] = datapoints_total
+    results_rel_dict["Total"] = outliers_total/datapoints_total
+    results_rel_dict["f1"] = f1_score(
+        results_fi["outlier"], results_fi["projection"])
+    results_rel_dict["accuracy"] = accuracy_score(
+        results_fi["outlier"], results_fi["projection"])
+    results_rel_dict["precision"] = precision_score(
+        results_fi["outlier"], results_fi["projection"])
+    results_rel_dict["recall"] = recall_score(
+        results_fi["outlier"], results_fi["projection"])
+    results_abs = pd.DataFrame([results_abs_dict])
+    results_rel = pd.DataFrame([results_rel_dict])
+
     # Write results to csv
     results_fi = results_fi.drop("Anomaly_Score", axis=1)
     if write_to_csv == True:
-        filename = "C:/Users/Florentina/Documents/Uni CSS/3. Semester/Results_ifor/Ifor_3std_4,16%_{}_{}_{}_{}_{}.csv".format(cont, 
-            max_feat, max_samp, n_est, random_state)
+        filename = Path("Results_ifor/Ifor_3std_4,16%_{}_{}_{}_{}_{}.csv".format(cont, max_feat, max_samp, n_est, random_state)).absolute()
         results_fi.to_csv(filename)
-
-    # Save overall results
-    results_abs_dict["Total"] = datapoints_total
-    results_rel_dict["Total"] = outliers_total/datapoints_total
-    results_abs = pd.DataFrame([results_abs_dict])
-    results_rel = pd.DataFrame([results_rel_dict])
     return results_abs, results_rel
+
 
 def cross_validation(params: dict, dataset_complete: pd.DataFrame,
                      write_to_csv: bool = False,
@@ -224,29 +243,29 @@ def cross_validation(params: dict, dataset_complete: pd.DataFrame,
     results_rel = pd.DataFrame(columns=cols)
     for c in combinations:
         abs, rel = algorithm(
-            dataset_complete, cont=c[0], max_feat=c[1], max_samp=c[2], n_est=c[3], random_state=c[4], write_to_csv = write_to_csv)
+            dataset_complete, cont=c[0], max_feat=c[1], max_samp=c[2], n_est=c[3], random_state=c[4], write_to_csv=write_to_csv)
 
         results_abs = pd.concat([results_abs, abs], ignore_index=True)
         results_rel = pd.concat([results_rel, rel], ignore_index=True)
-    
+
     if write_to_csv == True:
-        path_abs =  "C:/Users/Florentina/Documents/Uni CSS/3. Semester/Results_ifor/Ifor_abs.csv"
-        path_rel =  "C:/Users/Florentina/Documents/Uni CSS/3. Semester/Results_ifor/Ifor_rel.csv"
+        path_abs = Path("Results_ifor/Ifor_abs.csv").absolute()
+        path_rel = Path("Results_ifor/Ifor_rel.csv").absolute()
         results_abs.to_csv(path_abs)
         results_rel.to_csv(path_rel)
 
-    
     return results_abs, results_rel
 
 
 def main():
     hyper_params = {
-    'cont': [0.01, 0.05, 0.1],
-    'max_feat': [4, 16],
-    'max_samp': [10, 100],
-    'max_est': [10, 100],
-    'random_state': [0]}
+        'cont': [0.01, 0.05, 0.1],
+        'max_feat': [4, 16],
+        'max_samp': [10, 100],
+        'max_est': [10, 100],
+        'random_state': [0]}
 
     res_abs, res_rel = cross_validation(hyper_params, letter_df_complete, True)
-    
+
+
 main()
